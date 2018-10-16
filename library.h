9b76cdadf8af4ff7ae94eeefd2eadd852da17cc3
Andy Zhou <azhou4@stuy.edu>
2018-10-16 15:30:52 -0400

    added addsong in library not tested

--- library.h
+++ library.h
@@ -6,12 +6,22 @@
 struct song_node *library[27];
 
 struct song_node **make_library(){
-  for(int i = 0;i < 27;i++){
-    library[i] = malloc(sizeof(struct song_node));
+    for(int i = 0;i < 27;i++){
+    library[i] = make_list(0, 0);
   }
   return library;
 }
 
-struct song_node *add_song(char *artist, char *library){
-
+struct song_node **add_song(char *artist, char *song){
+  int i = 0;
+  while(i < 26){
+    if(!strncmp((char *)i + 65,song,1)){
+      library[i] = insert_ordered(library[i],artist,song);
+      i = 100;
+    }
+  }
+  if(i == 26){
+    library[i] = insert_ordered(library[26],artist,song);
+  }
+  return library;
 }
